5f9e784d72ecf2043b19ad50c26e111f12092605
Singh <Noopur.Singh@capco.com>
2019-04-11 22:53:00 +0530

    added registration and login code, requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login.login_manager import LoginManager
 
 microblogapp = Flask(__name__)
 microblogapp.config.from_object(Config)
 db = SQLAlchemy(microblogapp)
 migrate = Migrate(microblogapp, db)
+login = LoginManager(microblogapp)
+login.login_view = 'login' #view function that handles login
 
 from app import routes,models
--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,17 @@ Created on Apr 1, 2019
 
 @author: NSNO
 '''
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user,logout_user
 from app import microblogapp
-from app.forms import LoginForm
-
-@microblogapp.route('/hello')
-def hello():
-    return "hello"
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from flask_login import login_required
+from werkzeug.urls import url_parse
+from app import db
 
 @microblogapp.route('/index')
+@login_required #makes the url protected, makes login necessary
 def index():
     user = {"name":"noopur","age":23}
     posts = [
@@ -24,13 +26,49 @@ def index():
             'body': 'I think I will watch avengers..:)'    
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    #return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
+#This method renders login page on login GET request
+# and submits the user login data on POST login request
 @microblogapp.route('/login', methods=['GET','POST'])
 def login():
+    #a user that is logged in, and the user navigates to the /login URL of your application, restricted
+    if current_user.is_authenticated:#current_user to obtain the user object that represents the client of the request
+        return redirect(url_for('index'))
     form = LoginForm()
+    #In the GET request, no form, return False
+    #in the POST request, gather data, run form validators and return True/False
     if form.validate_on_submit():
-        ## just flashing message
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data,form.remember_me.data))
-        return redirect(url_for('index'))##the parameter is the method name not the url
+        #query the database for matching username, and return the first object as there will b only 1 user by that username
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.verify_password(form.password.data):
+            ## just flashing message
+            flash('Invalid username or password')
+            return redirect(url_for('login'))##the parameter is the method name not the url
+        #this method registers a user as logged on by setting the value of current_user in the session
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@microblogapp.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('/index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrats! you have been successfully registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+    
+@microblogapp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
